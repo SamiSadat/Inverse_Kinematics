f6d6cc2b00c3b735d1c436ba0cac34e55249bb97
Sami Sadat <72562659+SamiSadat@users.noreply.github.com>
2022-06-08 15:58:49 +0600

    Update ik.py

--- ik.py
+++ ik.py
@@ -1,5 +1,6 @@
 from cmath import sqrt
 import math
+import numpy as np
 l1 = 57.2
 l2 = 40.5
 l3= 0
@@ -22,3 +23,15 @@ theta_3 = math.degrees(math.acos(((l1*l1)+(l2*l2)-(d6*d6))/(2*l1*l2)))
 theta_3_updated = theta_3 - 11.5
 print('Theta_3: {}'.format(theta_3_updated))
 
+gg = np.cos(theta_3_updated)
+act2=math.sqrt((9.5*9.5)+(38.5*38.5)-(2*9.5*38.5*gg))
+pot_of_act2= (11.3418*act2) +176.299
+print(act2)
+
+print("pot_of_actuator 2: {}".format(pot_of_act2))
+
+gg2 = np.cos(theta_2_up)
+act1=math.sqrt((9.1*9.1)+(40*40)-(2*9.1*40*gg2))
+pot_of_act1= 18.0472*act1 -32.5536	
+print(act1)
+print("pot_of_actuator 1: {}".format(pot_of_act1))
